9fb5721641723ddebfa5b6f4020fb8c4c8be35fe
FreakingGrant <youngsgr@msu.edu>
2020-05-21 19:22:29 -0400

    Initial creation with index page.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>MixYours: The Cocktail Sharing Site</title>
+</head>
+
+<body>
+    <h1>Welcome to MixYours</h1>
+    <p><a href="#">Login</a></p>
+</body>
+</html>
---
+++ lib/MixYours/Site.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Object to represent our website.
+ */
+
+namespace MixYours;
+
+
+class Site
+{
+
+}
