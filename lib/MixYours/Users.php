7b8c6fba8a270ab3328f187ebac02e2677870fe2
FreakingGrant <youngsgr@msu.edu>
2020-05-26 14:29:33 -0400

    Started creating the Users table class

--- lib/MixYours/Users.php
+++ lib/MixYours/Users.php
@@ -17,4 +17,42 @@ class Users extends Table
     public function __construct(Site $site) {
         parent::__construct($site, "mixyours_user");
     }
+
+    /**
+     * Test for a valid login
+     * @param string $email User's email address
+     * @param string $password Password credential
+     * @return User object upon success, null on failure
+     */
+    public function login($email, $password) {
+        $sql =<<<SQL
+SELECT * FROM $this->tableName
+where email=?
+SQL;
+
+        // Prepare the SQL query
+        $pdo = $this->pdo();
+        $statement = $pdo->prepare($sql);
+
+        // Execute the command. If nothing is returned, the user does not exist and return null;
+        $statement->execute(array($email));
+        if($statement->rowCount() === 0) {
+            return null;
+        }
+
+        // User does exist, now check for password match //
+
+        $row = $statement->fetch(\PDO::FETCH_ASSOC);
+
+        // Get the encrypted password and salt from the record
+        $hash = $row['password'];
+        $salt = $row['salt'];
+
+        // Ensure it's correct. If it's not, return null
+        if($hash !== hash("sha256", $password . $salt)) {
+            return null;
+        }
+
+        return new User($row);
+    }
 }
